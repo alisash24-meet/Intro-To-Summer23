8ad5138b313c24594c99ddcdfd350274c3541936
alisash24-meet <alisa.shapovalov@gmail.com>
2024-07-14 17:15:53 +0300

    firstlab

---
+++ python_review.py
@@ -0,0 +1,11 @@
+import random
+temperatures = []
+
+for i in range(7):
+	n = random.randint(26,41)
+	temperatures.append(n)
+print(temperatures)
+
+days_of_the_week=["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
+
+
