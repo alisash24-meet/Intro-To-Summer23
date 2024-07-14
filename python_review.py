372add8daea806803d76cde12d2dbb08670b10b4
alisash24-meet <alisa.shapovalov@gmail.com>
2024-07-14 21:29:36 +0300

    big update

--- python_review.py
+++ python_review.py
@@ -7,5 +7,51 @@ for i in range(7):
 print(temperatures)
 
 days_of_the_week=["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
+good_days_count = []
 
 
+for i in range(7): 
+	if temperatures[i] % 2 == 0:
+		good_days_count.append(days_of_the_week[i])
+
+
+print(good_days_count)
+
+high = 0
+low = 0
+
+for i in range(7):
+	if temperatures[i] > temperatures[high]:
+		high = i
+	if temperatures[i] < temperatures[low]:
+		low = i
+
+highest_temp = temperatures[high]
+highest_temp_day = days_of_the_week[high]
+lowest_temp = temperatures[low]
+lowest_temp_day = days_of_the_week[low]
+
+average = 0
+
+for i in temperatures:
+	average += i
+
+average /= 7
+
+above_avg = []
+
+for i in temperatures:
+	if i > average:
+		above_avg.append(i)
+
+
+print("The Weather Report:")
+for i in range(7):
+	print("  ", days_of_the_week[i], ":", temperatures[i])
+	print("*")
+	print("*")
+	print("  ", "Shelly had 4 good days")
+	print("*")
+	print("*")
+	print("  ", highest_temp_day, ":", highest_temp)
+	print("  ", lowest_temp_day, ":", lowest_temp)
