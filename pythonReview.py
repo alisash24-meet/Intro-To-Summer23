4132d899d810151f24a2d7d12c32107618f0eca1
alisash24-meet <alisa.shapovalov@gmail.com>
2024-07-15 14:12:31 +0300

    pythonReviewUpdate1

---
+++ pythonReview.py
@@ -0,0 +1,16 @@
+def create_youtube_video(title, describtion):
+	video_dict = {
+	"title":"", "describtion": "", "likes": 0, "dislikes": 0, "comments":{
+	"username": ""
+	} 
+	}
+
+def like(create_youtube_video): 
+	if likes in create_youtube_video:
+		create_youtube_video["likes"] = create_youtube_video["likes"] + 1
+		return create_youtube_video
+
+def dislike(create_youtube_video): 
+	if dislikes in create_youtube_video:
+		create_youtube_video["dislikes"] = create_youtube_video["dislikes"] + 1
+		return create_youtube_video	
