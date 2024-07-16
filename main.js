4959f1070d094e68ad503d29c918ff544f3f047b
alisash24-meet <alisa.shapovalov@gmail.com>
2024-07-16 18:21:40 +0300

    BIG update

--- main.js
+++ main.js
@@ -1,2 +1,19 @@
 function changeBackgroundColor(color)
-	
+{
+	document.body.style.backgroundColor = color;
+//	console.log('hi');
+//	if (document.body.style.backgroundColor == "darkviolet")
+//		{
+//			console.log("in");
+//			document.body.style.backgroundColor = "blue";
+//		}
+//	else if (document.body.style.backgroundColor == "blue")
+//		{
+//			document.body.style.backgroundColor = "white";
+//		}
+//		else 
+//			{
+//				document.body.style.backgroundColor = "darkviolet";
+//			} didnt work for some reason on my computer (specifically)
+
+}
